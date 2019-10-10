48e2fc6a6d0ede5bdb1eb15f84064c1927aea147
David Frick <frick.programmer@gmail.com>
2019-10-09 22:12:20 -0400

    Combined the dash flask and peewee code and tidied up. Two issues exist: the flask-dash app using blueprints whereas the peewee does not, and peewee uses SQLlite whereas the dash flask uses SQLAlchemy

--- app/webapp.py
+++ app/webapp.py
@@ -1,54 +1,94 @@
-from flask import Blueprint
-from flask import redirect
-from flask import render_template
-from flask import request
-from flask import url_for
+from flask import (Blueprint, flash, redirect, render_template, request,
+                   Response, session, url_for, current_app)
+import functools
+import urllib
 from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
 from werkzeug.urls import url_parse
+from playhouse.flask_utils import get_object_or_404, object_list
+from playhouse.sqlite_ext import *
 
 from app.extensions import db
 from app.forms import LoginForm
 from app.forms import RegistrationForm
-from app.models import User
+from app.models import User, Entry
 
 server_bp = Blueprint('main', __name__)
 
 
 @server_bp.route('/')
 def index():
-    return render_template("index.html", title='Home Page')
+    search_query = request.args.get('q')
+    if search_query:
+        query = Entry.search(search_query)
+    else:
+        query = Entry.public().order_by(Entry.timestamp.desc())
+
+    # The `object_list` helper will take a base query and then handle
+    # paginating the results if there are more than 20. For more info see
+    # the docs:
+    # http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#object_list
+    return object_list(
+        'index.html',
+        query,
+        search=search_query,
+        check_bounds=False)
+
+
+# @server_bp.route('/login/', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('main.index'))
+#
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             error = 'Invalid username or password'
+#             return render_template('templates/login.html', form=form, error=error)
+#
+#         login_user(user, remember=form.remember_me.data)
+#         next_page = request.args.get('next')
+#         if not next_page or url_parse(next_page).netloc != '':
+#             next_page = url_for('main.index')
+#         return redirect(next_page)
+#
+#     return render_template('templates/login.html', title='Sign In', form=form)
 
 
 @server_bp.route('/login/', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            error = 'Invalid username or password'
-            return render_template('login.html', form=form, error=error)
-
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('main.index')
-        return redirect(next_page)
-
-    return render_template('login.html', title='Sign In', form=form)
+    next_url = request.args.get('next') or request.form.get('next')
+    if request.method == 'POST' and request.form.get('password'):
+        password = request.form.get('password')
+        # TODO: If using a one-way hash, you would also hash the user-submitted
+        # password and do the comparison on the hashed versions.
+        if password == current_app.config['ADMIN_PASSWORD']:
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            flash('Incorrect password.', 'danger')
+    return render_template('app/templates/login.html', next_url=next_url)
 
 
-@server_bp.route('/logout/')
-@login_required
+# @server_bp.route('/logout/')
+# @login_required
+# def logout():
+#     logout_user()
+#
+#     return redirect(url_for('main.index'))
+
+
+@server_bp.route('/logout/', methods=['GET', 'POST'])
 def logout():
-    logout_user()
-
-    return redirect(url_for('main.index'))
+    if request.method == 'POST':
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('app/templates/logout.html')
 
 
 @server_bp.route('/register/', methods=['GET', 'POST'])
@@ -66,3 +106,87 @@ def register():
         return redirect(url_for('main.login'))
 
     return render_template('register.html', title='Register', form=form)
+
+
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
+
+
+def _create_or_edit(entry, template):
+    if request.method == 'POST':
+        entry.title = request.form.get('title') or ''
+        entry.content = request.form.get('content') or ''
+        entry.published = request.form.get('published') or False
+        if not (entry.title and entry.content):
+            flash('Title and Content are required.', 'danger')
+        else:
+            # Wrap the call to save in a transaction so we can roll it back
+            # cleanly in the event of an integrity error.
+            try:
+                with current_app.database.atomic():
+                    entry.save()
+            except IntegrityError:
+                flash('Error: this title is already in use.', 'danger')
+            else:
+                flash('Entry saved successfully.', 'success')
+                if entry.published:
+                    return redirect(url_for('detail', slug=entry.slug))
+                else:
+                    return redirect(url_for('edit', slug=entry.slug))
+
+    return render_template(template, entry=entry)
+
+
+@server_bp.route('/create/', methods=['GET', 'POST'])
+@login_required
+def create():
+    return _create_or_edit(Entry(title='', content=''), 'create.html')
+
+
+@server_bp.route('/drafts/')
+@login_required
+def drafts():
+    query = Entry.drafts().order_by(Entry.timestamp.desc())
+    return object_list('index.html', query, check_bounds=False)
+
+
+@server_bp.route('/<slug>/')
+def detail(slug):
+    if session.get('logged_in'):
+        query = Entry.select()
+    else:
+        query = Entry.public()
+    entry = get_object_or_404(query, Entry.slug == slug)
+    return render_template('app/templates/detail.html', entry=entry)
+
+
+@server_bp.route('/<slug>/edit/', methods=['GET', 'POST'])
+@login_required
+def edit(slug):
+    entry = get_object_or_404(Entry, Entry.slug == slug)
+    return _create_or_edit(entry, 'edit.html')
+
+
+@server_bp.app_template_filter('clean_querystring')
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+    # We'll use this template filter in the pagination include. This filter
+    # will take the current URL and allow us to preserve the arguments in the
+    # querystring while replacing any that we need to overwrite. For instance
+    # if your URL is /?q=search+query&page=2 and we want to preserve the search
+    # term but make a link to page 3, this filter will allow us to do that.
+    querystring = dict((key, value) for key, value in request_args.items())
+    for key in keys_to_remove:
+        querystring.pop(key, None)
+    querystring.update(new_values)
+    return urllib.urlencode(querystring)
+
+
+@server_bp.errorhandler(404)
+def not_found(exc):
+    return Response('<h3>Not found</h3>'), 404
+
